a1bcea487e5dec532b2eda631e67ed929cf46cf1
FrostyGekyume <75546030+FrostyGekyume@users.noreply.github.com>
2021-01-02 00:45:48 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,35 @@
+const help = (prefix) => {
+	return `> *MorningStar* <
+*${prefix}sticker* ou *${prefix}stiker*  (faz animada tbm)\n
+*${prefix}toimg* (marque uma figurinha)\n
+
+> *Outros* <
+*${prefix}gtts*  (EX: *${prefix}gtts ja On2-chan*)\n
+*${prefix}loli*\n
+*${prefix}nsfwloli*\n
+*${prefix}simi*  (EX: *${prefix}simi yourmessage*)\n
+*${prefix}ocr*  (responder imagem ou enviar imagem com legenda)\n
+*${prefix}wait*  (responder imagem ou enviar imagem com legenda)\n
+*${prefix}escrever*\n
+*${prefix}blocklist*\n
+*${prefix}tiktokstalk (username)*\n
+
+> *Gropos* <
+*${prefix}listaadmins*\n
+*${prefix}ban*\n
+*${prefix}add*\n
+*${prefix}link*\n
+*${prefix}everyone*
+*${prefix}simih*  (1 Ativa 0 Desativa)\n
+*${prefix}bv  (1 Ativa 0 Desativa)\n\n
+
+meu número: wa.me/+5519996503657\n
+meu insta: https://instagram.com/frostyocruzado\n
+meu grupo só pq fodase: https://chat.whatsapp.com/EB4sKfEnhSKE7B0lKkjvHs\n
+
+desanimei`
+}
+
+exports.help = help
+
+
